aaac54a1b2222952694598b541777cd93d4c540b
shariorfarhan111 <118646614+shariorfarhan111@users.noreply.github.com>
2022-12-04 23:58:25 +0600

    Made two classes to receive request.

---
+++ src/main/java/com/todolist/todolist/request/AddTodoRequest.java
@@ -0,0 +1,20 @@
+package com.todolist.todolist.request;
+
+public class AddTodoRequest {
+    private String content;
+
+    public AddTodoRequest() {
+    }
+
+    public AddTodoRequest(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
